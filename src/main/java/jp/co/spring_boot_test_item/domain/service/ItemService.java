764d187ffb695b5a21dfa5f9fefdf1f12fdbc1ab
cm888best <cm.tech888best@gmail.com>
2020-06-15 08:54:37 +0900

    add annotation for transaction control

--- src/main/java/jp/co/spring_boot_test_item/domain/service/ItemService.java
+++ src/main/java/jp/co/spring_boot_test_item/domain/service/ItemService.java
@@ -3,6 +3,7 @@ package jp.co.spring_boot_test_item.domain.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.spring_boot_test_item.domain.model.Item;
 import jp.co.spring_boot_test_item.domain.repository.ItemRepository;
@@ -14,10 +15,12 @@ public class ItemService {
 
 	private final ItemRepository itemRepository;
 
+	@Transactional(readOnly = true)
 	public Item findByItemId(int itemId) {
 		return itemRepository.findByItemId(itemId);
 	}
 
+	@Transactional(readOnly = true)
 	public List<Item> findByCategoryId(int categoryId) {
 		return itemRepository.findByCategoryId(categoryId);
 	}
